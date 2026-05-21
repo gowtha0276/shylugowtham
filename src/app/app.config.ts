bbb4c71945714042a97cd6285db63d6647cdbbae
M E <shylueasu@gmail.com>
2026-05-21 23:54:48 +0530

    Enabled firebase analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
+
 import { routes } from './app.routes';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDhGjUBN5XlmkaiTmigD7HclMYbzQVtH70",
+  authDomain: "shyluwedsgowtham.firebaseapp.com",
+  projectId: "shyluwedsgowtham",
+  storageBucket: "shyluwedsgowtham.firebasestorage.app",
+  messagingSenderId: "210798746875",
+  appId: "1:210798746875:web:d23e5c54383e85c086a45e",
+  measurementId: "G-L33RHWBJTZ"
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAnalytics(() => getAnalytics())
+  ]
 };
